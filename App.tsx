4773bf3d98865070b3141f69ccc38350e9a8bb12
gautambuddha <nimesgautam@gmail.com>
2024-02-21 10:37:02 +0545

    feat(setup): moved tutorial components to learning folder

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
 import { SafeAreaView, StyleSheet, ScrollView, Text, View } from "react-native";
-import FlatCards from "./components/FlatCards";
-import ElevatedCards from "./components/ElevatedCards";
-import FancyCards from "./components/FancyCards";
-import ActionCard from "./components/ActionCard";
-import ContactList from "./components/ContactList";
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View style={styles.container}>
-          <FlatCards />
-          <ElevatedCards />
-          <FancyCards />
-          <ActionCard />
-          <ContactList />
-        </View>
+        <View style={styles.container}></View>
       </ScrollView>
     </SafeAreaView>
   );
